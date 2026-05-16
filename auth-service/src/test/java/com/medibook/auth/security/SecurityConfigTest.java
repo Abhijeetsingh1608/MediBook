1a95d7a1998bf725f9e3e94a9e134bc9f55d5f95
Abhijeetsingh1608 <kumarabhisingh2020@gmail.com>
2026-05-16 12:52:53 +0530

    fix: update SecurityConfigTest constructor args

--- auth-service/src/test/java/com/medibook/auth/security/SecurityConfigTest.java
+++ auth-service/src/test/java/com/medibook/auth/security/SecurityConfigTest.java
@@ -18,9 +18,10 @@ class SecurityConfigTest {
         OAuth2LoginSuccessHandler successHandler = mock(OAuth2LoginSuccessHandler.class);
         OAuth2LoginFailureHandler failureHandler = mock(OAuth2LoginFailureHandler.class);
         ClientRegistrationRepository clientRepo = mock(ClientRegistrationRepository.class);
+        com.medibook.auth.filter.InternalRequestFilter internalRequestFilter = mock(com.medibook.auth.filter.InternalRequestFilter.class);
 
         SecurityConfig config = new SecurityConfig(
-                jwtFilter, successHandler, failureHandler, clientRepo);
+                jwtFilter, successHandler, failureHandler, clientRepo, internalRequestFilter);
 
         AuthenticationConfiguration authConfig = mock(AuthenticationConfiguration.class);
         AuthenticationManager manager = mock(AuthenticationManager.class);
